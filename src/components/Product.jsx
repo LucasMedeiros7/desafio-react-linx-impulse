2e509c2f21bc92ddb52f63814668d1b477bf43ad
Lucasmedeiros7 <lucaasmedeiros7@gmail.com>
2022-08-30 17:31:52 -0300

    Botao comprar da sessao produtos

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,20 +2,23 @@ import styles from './Product.module.css';
 
 export function Product(props) {
   return (
-    <div>
+    <div className={styles.container}>
       <img className={styles.productImg} src={props.image} alt="" />
 
-      <div className={styles.nameAndDescription}>
-        <strong>{props.name}</strong>
-        <p>{props.description}</p>
-      </div>
+      <div className={styles.infoProduct}>
+        <div className={styles.nameAndDescription}>
+          <strong>{props.name}</strong>
+          <p>{props.description}</p>
+        </div>
+        <div className={styles.price}>
+          <span>De: R${props.oldPrice}</span>
+          <strong>Por: R${props.price}</strong>
+          <small>
+            ou {props.installments.count}x de R${props.installments.value}0
+          </small>
+        </div>
 
-      <div className={styles.price}>
-        <span>De: R${props.oldPrice}</span>
-        <strong>Por: R${props.price}</strong>
-        <small>
-          ou {props.installments.count}x de R${props.installments.value}0
-        </small>
+        <button>Comprar</button>
       </div>
     </div>
   );
